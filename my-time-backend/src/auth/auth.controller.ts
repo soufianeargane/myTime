88b2f9587930c2ceb0741bf6b936752600340ae7
soufianeargane <soufianeargane800@gmail.com>
2024-01-04 14:59:23 +0100

    added UML and checked in user already exists

--- my-time-backend/src/auth/auth.controller.ts
+++ my-time-backend/src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreatedUserDto } from './dto/create-user.dto';
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('register')
   async register(@Body() createdUserDto: CreatedUserDto) {
-    console.log('userDto', createdUserDto);
-    return true;
-
-    return await this.authService.create(createdUserDto);
+    const existUser = await this.authService.getUserByEmail(
+      createdUserDto.email,
+    );
+    console.log({ existUser });
+    if (existUser) {
+      throw new UnauthorizedException('User already exists');
+    }
+    await this.authService.create(createdUserDto);
+    return 'createdUser';
   }
 }
--- my-time-backend/src/auth/auth.service.ts
+++ my-time-backend/src/auth/auth.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
   async create(userDto: any) {
-    const createdUser = new this.userModel(userDto);
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(userDto.password, salt);
+    const data = {
+      ...userDto,
+      password: hashPassword,
+    };
+
+    const createdUser = new this.userModel(data);
     return await createdUser.save();
-    // return 'createdUser';
+  }
+
+  async getUserByEmail(email: string) {
+    return await this.userModel.findOne({ email }).exec();
   }
 }
